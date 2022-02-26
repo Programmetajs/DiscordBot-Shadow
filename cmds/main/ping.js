cc817ed258ff0def82230b35dc012d634ecf2f3e
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-02-26 14:36:00 +0800

    Update ping.js

--- cmds/main/ping.js
+++ cmds/main/ping.js
@@ -1,5 +1,37 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
   name: "ping", // the command name - remember this is a file template for making a new command
-  run: async ()
+  run: async (app, m, args, prefix, id) => {
+    m.reply(
+      {
+        content: "Content - Pong!"
+      }
+    ).then(() => {
+      m.channel.send(
+        embeds: [
+          new MessageEmbed()
+          .setColor("BLURPLE")
+          .setDescription("Embed - Pong!")
+        ]
+      );
+      setTimeout(() => {
+        m.channel.send(
+          embeds: [
+            new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription("Button Embed - Pong!")
+          ],
+          components: [
+            new MessageActionRow()
+            .addComponents(
+              new MessageButton()
+              .setStyle("SUCCESS")
+              .setLabel("ws")
+              .setDisabled(true)
+            )
+          ]
+        )
+      }, 1000);
+    })
+  }
 }
