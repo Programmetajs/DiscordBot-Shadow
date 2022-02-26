60fcccd841f843bee3da288b08b0d1111963d78e
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-02-26 14:49:26 +0800

    simple ping-pong command

--- cmds/main/ping.js
+++ cmds/main/ping.js
@@ -6,32 +6,6 @@ module.exports = {
       {
         content: "Content - Pong!"
       }
-    ).then(() => {
-      m.channel.send(
-        embeds: [
-          new MessageEmbed()
-          .setColor("BLURPLE")
-          .setDescription("Embed - Pong!")
-        ]
-      );
-      setTimeout(() => {
-        m.channel.send(
-          embeds: [
-            new MessageEmbed()
-            .setColor("BLUE")
-            .setDescription("Button Embed - Pong!")
-          ],
-          components: [
-            new MessageActionRow()
-            .addComponents(
-              new MessageButton()
-              .setStyle("SUCCESS")
-              .setLabel("ws")
-              .setDisabled(true)
-            )
-          ]
-        )
-      }, 1000);
-    })
+    );
   }
 }
