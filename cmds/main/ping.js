58c181dfe1dc0e2cc2ee7fa38b8e4c89901abc15
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-02-26 12:59:04 +0800

    a simple template for your commands
    a simple ping webSocket command

---
+++ cmds/main/ping.js
@@ -0,0 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "ping", // the command name - remember this is a file template for making a new command
+  run: async ()
+}
