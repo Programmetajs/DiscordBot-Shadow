55ab1811676f9aedd7ccda7274341fe088ecb7d0
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-02-28 13:33:54 +0800

    (```keep_alive.js```) http  runner

--- keep_alive.js
+++ keep_alive.js
@@ -1,13 +1,12 @@
 const http = require("express");
 const run = http();
-const port = 3000;
 (async () => {
   try {
     run.get("/", async (req, res) => {
       res.send("keep_alive")
     });
     const ch = require("chalk");
-    run.listen(port, async () => {
+    run.listen(3000, async () => {
       console.log(ch.cyan(".. || Successfully Executed Keep_alive file || .."))
     });
   } catch (error) {
