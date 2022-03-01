9a8e11a1d9505287fd8711a36669dbdc7a048427
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-03-01 12:52:44 +0800

    (```ping.js```) a simple command

--- cmds/main/ping.js
+++ cmds/main/ping.js
@@ -1,11 +1,38 @@
+// ping-pong command in embed command
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
   name: "ping", // the command name - remember this is a file template for making a new command
   run: async (app, m, args, prefix, id) => {
-    m.reply(
+    m.author.send(
       {
-        content: "Content - Pong!"
+        embeds: [
+          new MessageEmbed()
+          .setColor("BLURPLE")
+          .setDescription(":tada:Pong!")
+          .setFooter(
+            {
+              text: `[ ${prefix}ping | ${m.author.tag} ] `,
+              iconURL: app.user.avatarURL()
+            }
+          )
+          .setTimestamp(Date.now())
+        ],
+        components: [
+          new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+            .setStyle("SECONDARY")
+            .setLabel(`Sent From: ${m.guild.name}`)
+            .setCustomId("sent-from")
+          )
+        ]
       }
-    );
+    ).catch(() => {
+      m.reply(
+        {
+          content: ":herb:Please Open your DMs to execute this command!"
+        }
+      )
+    });
   }
 }
