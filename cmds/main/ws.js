ff46b194ad2dbb500fac3946a9abcea125d82c8f
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-03-01 12:58:45 +0800

    (```ws.js```) a simple websocket command

--- cmds/main/ws.js
+++ cmds/main/ws.js
@@ -1,10 +1,41 @@
+// WebSocket Ping or speed in embed command.
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
   name: "ws",
   run: async (app, m, args, prefix, id) => {
-    m.reply(
+    m.react("✅");
+    m.author.send(
       {
-        content: `WebSocket Ping \`${app.ws.ping}ms\``
+        embeds: [
+          new MessageEmbed()
+          .setColor("BLURPLE")
+          .setTitle(`${app.user.username} WebSocket Ping!`)
+          .setDescription(`My Current WebSocket Ping is \`${app.ws.ping}ms\``)
+          .setFooter(
+            {
+              text: `[ ${prefix}ws | ${m.author.tag} ] `,
+              iconURL: app.user.avatarURL()
+            }
+          )
+          .setTimestamp(Date.now())
+        ],
+        components: [
+          new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+            .setStyle("SECONDARY")
+            .setLabel(`Sent From: ${m.guild.name}`)
+            .setCustomId("send-from")
+            .setDisabled(true)
+          )
+        ]
       }
-    );
+    ).catch(() => {
+      m.reply(
+        {
+          content: ""
+        }
+      )
+    })
   }
 }
