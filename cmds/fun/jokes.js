b2c42acadcbd93bf0ba4d531c5448017e8c57158
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-03-01 13:22:06 +0800

    (```jokes.js```) a simple fun command

--- cmds/fun/jokes.js
+++ cmds/fun/jokes.js
@@ -1,6 +1,43 @@
 // a simple node-fetch command in embed
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
+const fetch = require("node-fetch")
 module.exports = {
   name: "jokes",
-  run: async ()
+  run: async (app, m, args, prefix, id, embed_default_color) => {
+    const { joke } = await fetch("https://some-random-api.ml/joke").then((response) => response.json());
+    m.react("✅");
+    m.author.send(
+      {
+        embeds: [
+          new MessageEmbed()
+          .setColor(embed_default_color)
+          .setTitle("Funny Jokes!")
+          .setDescription(joke)
+          .setFooter(
+            {
+              text: `[ ${prefix}jokes | ${m.author.tag} ] `,
+              iconURL: app.user.avatarURL()
+            }
+          )
+          .setTimestamp(Date.now())
+        ],
+        components: [
+          new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+            .setStyle("SECONDARY")
+            .setLabel(`Sent From: ${m.guild.name}`)
+            .setCustomId("sent-from")
+            .setDisabled(true)
+          )
+        ]
+      }
+    ).catch(() => {
+      m.reply(
+        {
+          content: ":herb:Please Open your DMs to execute this command!"
+        }
+      )
+    })
+  }
 }
