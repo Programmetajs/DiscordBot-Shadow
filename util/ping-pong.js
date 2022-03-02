34f4830cf65bccb63d046d9ae36e2e70b6140728
GalaxyTechnician#9634 <94289792+GalaxyTechnician@users.noreply.github.com>
2022-03-02 09:15:37 +0800

    (```ping-pong.js```) a simple slash command

--- util/ping-pong.js
+++ util/ping-pong.js
@@ -1,12 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-module.exports = {
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { prefix, id, embed_default_color } = require("../src/config.json");
+module.exports = (app) => {
   data: new SlashCommandBuilder()
   .setName("ping")
   .setDescription("WebSocket Ping Command."),
   async execute(interaction) {
     await interaction.reply(
       {
-        content: "Pong!"
+        content: `**${interaction.user.username}**#${interaction.user.discriminator}`,
+        embeds: [
+          new MessageEmbed()
+          .setColor(embed_default_color)
+          .setTitle(`${app.user.username} WebSocket Ping (/)`)
+          .setDescription(`${app.user.username} current WebSocket Ping was \`${app.ws.ping}ms\``)
+          .setTimestamp(Date.now())
+        ],
+        components: [
+          new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+            .setStyle("SECONDARY")
+            .setLabel(`Sent From: ${interaction.guild.name}`)
+            .setCustomId("sent-from")
+            .setDisabled(true)
+          )
+        ]
       }
     )
   }
